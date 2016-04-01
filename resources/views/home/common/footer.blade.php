03acd7537b55718fe53e00738d8ccbf2167c23a7
Will <will.zhangweilin@gmail.com>
2016-04-01 19:01:32 +0800

    404 page UI

--- resources/views/home/common/footer.blade.php
+++ resources/views/home/common/footer.blade.php
@@ -1,50 +1,6 @@
 <!-- footer -->
 <div class="footer">
 	<div class="container">
-		<!-- <div class="col-md-4 social">
-			<h4>Social</h4>
-			<p>Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. </p>
-			<ul>
-				<li><a href="#"><i class="facebok"> </i></a></li>
-				<li><a href="#"><i class="twiter"> </i></a></li>
-				<li><a href="#"><i class="rss"> </i></a></li>
-				<div class="clearfix"></div>
-			</ul>
-		</div> -->
-		<!-- <div class="col-md-4 information">
-			<h4>Information</h4>
-			<p>Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. </p>
-			<p>Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi. Aliquam erat ac ipsum.</p>
-		</div> -->
-		<!-- <div class="col-md-4 searby">
-			<h4>Search by</h4>
-			<div class="col-md-6 by1">
-					<ul>
-						<li><a href="#">Maecenas (2)</a></li>
-						<li><a href="#">Curabitur et ligula (12)</a></li>
-						<li><a href="#">Ut molestie (3)</a></li>
-						<li><a href="#">Pellentesque facilisis (1)</a></li>
-						<li><a href="#">Integer aliquam (0)</a></li>
-						<li><a href="#">Lorem ipsum (1)</a></li>
-						<li><a href="#">Etiam ullamcorper (3)</a></li>
-						<li><a href="#">Vestibulum commodo (9)</a></li>
-						<li><a href="#">Quisque lorem (1)</a></li>
-						<li><a href="#">Vestibulum (23)</a></li>
-					</ul>
-				</div>
-				<div class="col-md-6 by1">
-					<ul>
-						<li><a href="#">Maecenas (2)</a></li>
-						<li><a href="#">Curabitur et ligula (12)</a></li>
-						<li><a href="#">Ut molestie (3)</a></li>
-						<li><a href="#">Pellentesque facilisis (1)</a></li>
-						<li><a href="#">Integer aliquam (0)</a></li>
-						<li><a href="#">Lorem ipsum (1)</a></li>
-						<li><a href="#">Etiam ullamcorper (3)</a></li>
-					</ul>
-				</div>
-				<div class="clearfix"> </div>
-		</div> -->
 		<div class="clearfix"></div>
 		<div class="bottom">
 			<p>Copyright &copy; 2016-2018.Will All rights reserved.<a href="http://www.miitbeian.gov.cn">{{ setting('filling') }}</a>

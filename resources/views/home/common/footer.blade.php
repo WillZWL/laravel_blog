827722f4f11e43c3535743228bb9cf110b39e4b6
Will <will.zhangweilin@gmail.com>
2016-04-01 08:52:15 +0800

    add Bootstrap at footer

--- resources/views/home/common/footer.blade.php
+++ resources/views/home/common/footer.blade.php
@@ -49,7 +49,7 @@
 		<div class="bottom">
 			<p>Copyright &copy; 2016-2018.Will All rights reserved.<a href="http://www.miitbeian.gov.cn">{{ setting('filling') }}</a>
 			<br/>
-			Developed By <a href="/abouts" target="_blank">Will Zhang</a>,&nbsp;&nbsp;&nbsp;&nbsp;Powered By <a href="https://laravel.com/">Laravel</a><br/>
+			Developed By <a href="/abouts" target="_blank">Will Zhang</a>,&nbsp;&nbsp;Powered By <a href="https://laravel.com/">Laravel</a>&nbsp;&nbsp;&&&nbsp;&nbsp;<a href="http://getbootstrap.com/">Bootstrap</a><br/>
 			Run On <a href="https://www.aliyun.com">Aliyun's</a> Elastic Compute Service
 		</div>
 	</div>
